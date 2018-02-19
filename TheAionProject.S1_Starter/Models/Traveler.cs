9d8613e254273a688e4dac35a7d7220883dff9c3
Tyler Kirschke <kirsch14@ad.nmc.edu>
2018-02-19 15:13:04 -0500

    Add project files.

---
+++ TheAionProject.S1_Starter/Models/Traveler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject
+{
+    /// <summary>
+    /// the character class the player uses in the game
+    /// </summary>
+    public class Traveler : Character
+    {
+        #region ENUMERABLES
+
+
+        #endregion
+
+        #region FIELDS
+
+        private string _homePlanet;
+        
+
+        #endregion
+
+
+        #region PROPERTIES
+
+        public string HomePlanet
+        {
+            get { return _homePlanet; }
+            set { _homePlanet = value; }
+        }
+
+        #endregion
+
+
+        #region CONSTRUCTORS
+
+        public Traveler()
+        {
+
+        }
+
+        public Traveler(string name, RaceType race, int spaceTimeLocationID) : base(name, race, spaceTimeLocationID)
+        {
+
+        }
+
+        #endregion
+
+
+        #region METHODS
+
+
+        #endregion
+    }
+}
